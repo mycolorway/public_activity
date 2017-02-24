2a1fce779dec258a53252a08281385afbc898ac1
Jeff Lai <winfield301@gmail.com>
2017-02-24 12:20:20 +0800

    Add paranoia support for activity

--- lib/generators/public_activity/migration/templates/migration.rb
+++ lib/generators/public_activity/migration/templates/migration.rb
@@ -5,9 +5,10 @@ class CreateActivities < ActiveRecord::Migration
     create_table :activities do |t|
       t.belongs_to :trackable, :polymorphic => true
       t.belongs_to :owner, :polymorphic => true
+      t.belongs_to :recipient, :polymorphic => true
       t.string  :key
       t.text    :parameters
-      t.belongs_to :recipient, :polymorphic => true
+      t.datetime :deleted_at
 
       t.timestamps null: false
     end
@@ -15,6 +16,7 @@ class CreateActivities < ActiveRecord::Migration
     add_index :activities, [:trackable_id, :trackable_type]
     add_index :activities, [:owner_id, :owner_type]
     add_index :activities, [:recipient_id, :recipient_type]
+    add_index :activities, :deleted_at
   end
   # Drop table
   def self.down
