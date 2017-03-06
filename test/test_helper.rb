588b47b067ea6b7b6af000a2180b24e6e25f3c77
Jeff Lai <winfield301@gmail.com>
2017-03-06 14:50:48 +0800

    Fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,6 +19,7 @@ require 'public_activity'
 require 'public_activity/testing'
 require 'pry'
 require 'minitest/autorun'
+require 'paranoia'
 
 ENV['PA_ORM'] ||= "active_record"
 PublicActivity.configure do |config|
